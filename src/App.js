6d3570936eb76d9ae54cee50072d4842982547e0
Nikhil <nikhil@deb.nikhil>
2023-09-21 18:30:07 +0530

    Addition: Footer Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Hero from './components/heroSection';
+import Footer from './components/footerSection'
 
 function App() {
   return (
     <div>
       {/* Hero Section */}
       <Hero/>
+
+      {/* Footer Section */}
+      <Footer/>
     </div>
   );
 }
